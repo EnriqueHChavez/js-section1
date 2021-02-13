b45c6616a95015ffb57f010ebde4dd5c5359c9fb
Enrique Chavez <ehernandezchavez51@gmail.com>
2021-02-13 01:30:37 -0800

    adding pokemon step 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('hello world');
-let favoriteFood = 'pizza'
-document.write('My favorite food is ' + favoriteFood);
+let pokemonList = [
+    {Pokemon: 'Onix', height: 8.8 , type: 'rock', 'ground', Evolution: 'Steelix' },
+    {Pokemon: 'Infernape', height: 1.2, type: 'fire', 'fighting', Evolution:'Monferno'},
+    {Pokemon: 'Metagross', height: 1.6. type: 'pyschic', 'steel', Evolution: 'Metang'}
+];
