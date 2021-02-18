7e44b19ac76a2ec8fb73294081cd7bb616bb263b
Enrique Chavez <ehernandezchavez51@gmail.com>
2021-02-17 18:48:38 -0800

    javascript loops

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ let pokemonList = [
     {pokemonName: 'Infernape', height: 1.2, type: ['fire', 'fighting'], evolution:'Monferno'},
     {pokemonName: 'Metagross', height: 1.6, type:[ 'pyschic', 'steel'], evolution: 'Metang'}
 ];
+
+let pokemonNames = [ 'Onix ',  ' Infernape ',  ' Metagross ' ];
+let pokemonHeights = [8.8, 1.2, 1.6];
+for (let i = 0; i < pokemonNames.length ; i++ ){
+    document.write(pokemonNames[i] + 'Height:' + pokemonHeights[i]);
+
+    if (pokemonHeights > 1.5) {
+        document.write(' Wow, thats big !');
+    }
+}
