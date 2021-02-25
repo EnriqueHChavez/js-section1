f1b075c31366b687f9be750f5abda1be7369d316
Enrique Chavez <ehernandezchavez51@gmail.com>
2021-02-25 15:47:00 -0800

    javascript review

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,56 @@ let pokemonList = [
     {pokemonName: 'Metagross', height: 1.6, type:[ 'pyschic', 'steel'], evolution: 'Metang'}
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(
-      pokemonList[i].pokemonName + " (Height: " + pokemonList[i].height + ")"
-    );
+pokemonList.forEach(function(names) {
+  document.write(
+    names.pokemonName + " (Height: " + names.height + ")"
+);
+
   
-    if (pokemonList[i].height > 1.5) {
+    if (names.height > 1.5) {
       document.write(" - Wow, that's big !" + "</br>" + "</br>");
     } else {
       document.write("</br>" + "</br>");
     }
-  }
+});
   
+
+
+
+
+/* Is this how I wrap into IIFE ? 
+
+(function () {
+ let pokemonList = [
+    {pokemonName: 'Onix', height: 8.8 , type: ['rock', 'ground'], evolution: 'Steelix' },
+    {pokemonName: 'Infernape', height: 1.2, type: ['fire', 'fighting'], evolution:'Monferno'},
+    {pokemonName: 'Metagross', height: 1.6, type:[ 'pyschic', 'steel'], evolution: 'Metang'}
+];
+  // put your code in here
+})(); */
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+// Previous code
+//for (let i = 0; i < pokemonList.length; i++) {
+ //  document.write(
+   // pokemonList[i].pokemonName + " (Height: " + pokemonList[i].height + ")"
+  // );
+
+
+  //let pokemonList = [
+  //  {pokemonName: 'Onix', height: 8.8 , type: ['rock', 'ground'], evolution: 'Steelix' },
+  //  {pokemonName: 'Infernape', height: 1.2, type: ['fire', 'fighting'], evolution:'Monferno'},
+  //  {pokemonName: 'Metagross', height: 1.6, type:[ 'pyschic', 'steel'], evolution: 'Metang'}
+//];
