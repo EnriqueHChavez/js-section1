240812c4cf190131628fefeae4eb2596cf17e593
Enrique Chavez <ehernandezchavez51@gmail.com>
2021-03-05 20:59:40 -0800

    javascript additions

--- js/scripts.js
+++ js/scripts.js
@@ -21,27 +21,25 @@ let pokemonRepository = (function () {
   ];
 
   // create add function to add a pokemon
-let pokemonRepository = (function() {
-  let pokemonList = [];
+
   // create getAll function to retrieve the entire pokemonList array
 
+  // return your add and getAll functions here as key/value pair
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+    
+    function getAll() {
+      return pokemonList;
+    }
 
-    // return your add and getAll functions here as key/value pair
     return {
-      add: function (pokemon){
-        pokemonList.push(pokemon);
-      },
-      getALL : function() {
-        return pokemonList;
-      } 
-  };
+      getAll: getAll,
+      add : add
+    };
 })();
 
-// access your pokemonList via your forEach loop and display the Pokemon exctly how you did in the last lesson - number 3 of part 2 shows you how to do this
-pokemonList.forEach(function(names) {
-  document.write(
-    names.pokemonName + " (Height: " + names.height + ")" );
-});
+pokemonRepository.getAll(); 
 // the list of Pokemon should be displayed exactly how it was before, the only thing that will be different is your code
 
 
